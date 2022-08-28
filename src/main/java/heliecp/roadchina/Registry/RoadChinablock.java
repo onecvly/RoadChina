f8fde7252f0860c75125714612d9e2197e102349
onecvly <102892415+onecvly@users.noreply.github.com>
2022-08-28 21:39:48 +0800

    Delete src/main directory

--- src/main/java/heliecp/roadchina/Registry/RoadChinablock.java
+++
@@ -1,9 +0,0 @@
-package heliecp.roadchina.Registry;
-
-import net.minecraft.block.Block;
-
-public class RoadChinablock extends Block {
-    public RoadChinablock(Settings settings){
-        super(settings);
-    }
-}
